8af4a95fa32e04110993a4439d79473cd405b568
Richard Backhouse <richard@Richards-MacBook-Pro-2.local>
2022-11-02 22:33:41 +0000

    as good as it gets just now...

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,6 +10,6 @@ from models.order_list import orders
 def index():
     return render_template('index.html', title ='Home', orders=orders)
 
-@app.route('/orders/1')
-def order():
-    return render_template('order.html', title ='Order', orders=orders)
+@app.route('/orders/<index>')
+def order(index):
+    return render_template('order.html', title ='Order', order=orders[int(index)])
